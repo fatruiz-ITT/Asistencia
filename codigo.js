3f0755ae84a714a65a768a66669c685e135543b3
fatruiz-ITT <ruizgarciafatimadejesus@gmail.com>
2024-12-15 18:01:55 -0800

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -155,7 +155,7 @@ function cargarAlumnos(data, materiaSeleccionada, grupoSeleccionado) {
             <td>${alumno}</td>
             <td>${nombre}</td>
             <td style="display: flex; align-items: center; justify-content: center;"><input type="checkbox" ${asistio === 'Sí' ? 'checked' : ''}></td>
-            <td>${materiaSeleccionada}</td>
+            <td>${colocarFechaAsistencia}</td>
         `;
         tablaAlumnos.appendChild(tr);
     });
