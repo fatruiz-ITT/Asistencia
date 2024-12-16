858390487416c0b5729e9682c610c0ac40956722
fatruiz-ITT <ruizgarciafatimadejesus@gmail.com>
2024-12-15 18:43:54 -0800

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -24,6 +24,7 @@ document.getElementById('guardar-lista').addEventListener('click', async () => {
     try {
         const response = await fetch(url, {
             method: 'POST',
+            mode: 'no-cors',
             headers: {
                 'Content-Type': 'application/json',
             },
